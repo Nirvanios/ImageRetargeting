6b9a009c03b5daeae1200c85b5fee168f5f85cc1
Igor Frank <ifrank@seznam.cz>
2020-03-29 17:51:03 +0200

    added type

--- Point.py
+++ Point.py
@@ -10,6 +10,7 @@ class PointType(Enum):
 
 
 class Point:
-    def __init__(self, x: int = 0, y: int = 0):
+    def __init__(self, x: int = 0, y: int = 0, type: PointType = PointType.NONE):
         self.x = x
         self.y = y
+        self.type = type
