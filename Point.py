d799a39c2658196e5ea7fcefdb8750c9db375bb2
Igor Frank <ifrank@seznam.cz>
2020-03-29 17:43:54 +0200

    added Point class

---
+++ Point.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class PointType(Enum):
+    NONE = 0
+    CORNER = 1
+    BORDER = 2
+    SALIENCY = 3
+    OTHER = 4
+
+
+class Point:
+    def __init__(self, x: int = 0, y: int = 0):
+        self.x = x
+        self.y = y
